54147f868a4bc5b9cbab26459a97dd9f62e86205
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-26 16:24:26 +0530

    Binary Tree created with 13 nodes

--- src/com/bridgelabz/bst/MyBinaryTreeRunner.java
+++ src/com/bridgelabz/bst/MyBinaryTreeRunner.java
@@ -7,6 +7,16 @@ public class MyBinaryTreeRunner {
 		binarytree.add(56);
 		binarytree.add(30);
 		binarytree.add(70);
+		binarytree.add(22);
+		binarytree.add(40);
+		binarytree.add(60);
+		binarytree.add(95);
+		binarytree.add(11);
+		binarytree.add(65);
+		binarytree.add(3);
+		binarytree.add(16);
+		binarytree.add(63);
+		binarytree.add(67);
 		
 		System.out.println(binarytree);
 		//int size = binarytree.getSize();
