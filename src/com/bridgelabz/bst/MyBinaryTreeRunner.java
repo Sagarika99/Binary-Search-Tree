bc43176f932bf17d04ee83229d72917b69645155
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-26 16:12:01 +0530

    First Commit

---
+++ src/com/bridgelabz/bst/MyBinaryTreeRunner.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.bst;
+
+public class MyBinaryTreeRunner {
+	public static void main(String[] args) {
+		
+		MyBinaryTree<Integer> binarytree = new MyBinaryTree<>();
+		binarytree.add(56);
+		binarytree.add(30);
+		binarytree.add(70);
+		
+		System.out.println(binarytree);
+		//int size = binarytree.getSize();
+		//System.out.println(size);
+	}
+	
+
+}
